e17af3052ed47633c0fea0adc849c61feafc1b3e
codesajinda <csmasterpiece@gmail.com>
2015-12-25 19:25:12 -0600

    * Added space

--- Services/SoundLoaderService.js
+++ Services/SoundLoaderService.js
@@ -1,5 +1,6 @@
 angular.module('midiPad').service('SoundLoaderService', ['$http', function($http) {
 	var soundLoaderService = {};
+	
 	soundLoaderService.loadSound = function(url){
 		return $http.get('/Sounds/' + url, {responseType:'arraybuffer'});
 	}
